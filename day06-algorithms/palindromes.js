c51093f577f55eb2892c1263b83b22023c57fc85
zofejes <zofejes@gmail.com>
2018-05-01 15:23:31 +0200

    day06 - palindromes.js assignment done

--- day06-algorithms/palindromes.js
+++ day06-algorithms/palindromes.js
@@ -5,10 +5,30 @@
 
 
 function palindrome(word) {
-    // ...
-}
+    // if word.length < 1 : that means we were able to slice 
+    // away all available characters so word.length is 0
+    // let's return this in a way that it's truthy, so negate it
+    if (word.length < 1) return !word.length
 
+    // if the word's first and last characters are the same, 
+    // slice 'em off and test again
+    if (word[0] == word[word.length-1]) {
+        return palindrome(word.slice(1,-1))
+    } else {
+        // if first and last chars are not the same, return false
+        return false
+    }
+}
 // test
-console.log(`test palindrome 1: -> ${palindrome('noon')}`)
-console.log(`test palindrome 2: -> ${palindrome('civic')}`)
-console.log(`test palindrome 3: -> ${!palindrome('test')}`)
+console.log(`test palindrome 1  - noon       : -> ${palindrome('noon')}`)
+console.log(`test palindrome 2  - civic      : -> ${palindrome('civic')}`)
+console.log(`test palindrome 3  - test       : -> ${palindrome('test')}`)
+console.log(`test palindrome 4  - reviver    : -> ${palindrome('reviver')}`)
+console.log(`test palindrome 5  - abcdedcba  : -> ${palindrome('abcdedcba')}`)
+console.log(`test palindrome 6  - xxxfxxx    : -> ${palindrome('1234321')}`)
+console.log(`test palindrome 7  - aabaa      : -> ${palindrome('aabaa')}`)
+console.log(`test palindrome 8  - aabba      : -> ${palindrome('aabba')}`)
+console.log(`test palindrome 9  - abcdef     : -> ${palindrome('abcdef')}`)
+console.log(`test palindrome 10 - xfxfxf     : -> ${palindrome('xfxfxf')}`)
+
+
