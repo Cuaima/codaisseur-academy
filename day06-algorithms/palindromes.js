9aa957861aa5c1a9dbc3d4a860fc2b4b6c203d89
zofejes <zofejes@gmail.com>
2018-05-01 14:44:33 +0200

    day06 - started on palindromes assignment

---
+++ day06-algorithms/palindromes.js
@@ -0,0 +1,14 @@
+//ASSIGNMENT:
+//Write a function to check if a word can be read forwards and backwards 
+//as the same word, using recursion. These are called palindromes. 
+//A long palindrome is: reviver.
+
+
+function palindrome(word) {
+    // ...
+}
+
+// test
+console.log(`test palindrome 1: -> ${palindrome('noon')}`)
+console.log(`test palindrome 2: -> ${palindrome('civic')}`)
+console.log(`test palindrome 3: -> ${!palindrome('test')}`)
